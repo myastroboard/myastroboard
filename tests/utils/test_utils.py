a3d705932097956b24c8c730a8aa011465558984
Gloup <12786921+WorldOfGZ@users.noreply.github.com>
2026-07-22 10:29:16 +0200

    Refactor logging in parse_iso_to_utc tests to use direct function calls

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -9,7 +9,6 @@ import yaml
 from unittest.mock import patch
 
 # Import the functions to test
-import utils as utils_module
 from utils import (
     IndentDumper,
     _NumpySafeEncoder,
@@ -491,20 +490,20 @@ class TestParseIsoToUtcLogsFallback:
     """
 
     def test_logs_warning_for_unparseable_value(self):
-        with patch.object(utils_module, "logger") as mock_logger:
-            utils_module.parse_iso_to_utc("not-a-date")
+        with patch("utils.logger") as mock_logger:
+            parse_iso_to_utc("not-a-date")
 
         assert mock_logger.warning.called
         assert "not-a-date" in str(mock_logger.warning.call_args)
 
     def test_logs_warning_for_none(self):
-        with patch.object(utils_module, "logger") as mock_logger:
-            utils_module.parse_iso_to_utc(None)
+        with patch("utils.logger") as mock_logger:
+            parse_iso_to_utc(None)
 
         assert mock_logger.warning.called
 
     def test_does_not_log_for_valid_value(self):
-        with patch.object(utils_module, "logger") as mock_logger:
-            utils_module.parse_iso_to_utc("2026-01-01T12:00:00+02:00")
+        with patch("utils.logger") as mock_logger:
+            parse_iso_to_utc("2026-01-01T12:00:00+02:00")
 
         assert not mock_logger.warning.called
